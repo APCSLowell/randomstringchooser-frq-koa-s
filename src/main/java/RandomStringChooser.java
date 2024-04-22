2964024a75de5286cbbf6f881c9634351bfee548
koa-s <143123833+koa-s@users.noreply.github.com>
2024-04-22 12:11:47 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,6 +1,28 @@
 import java.util.*;
 public class RandomStringChooser
 {
+  private String[] list;
+  private ArrayList<String> aList;
+  public RandomStringChooser(String[] sString)
+  {
+    aList = new ArrayList<String>();
+    for(int i = 0; i<list.length(); i++)
+      {
+        aList.add(list[i]);
+      }
+  }
+  public String getNext()
+  {
+    if(aList.size()==0)
+    {
+      return "NONE";
+    }
+    int num = (int)(Math.random()*aList.size())
+    String sString = aList.get(num);
+    aList.remove(num)
+      return sString;
+  }
+  
   /* to be implemented in part (a) */
   
   //Heads up! 
